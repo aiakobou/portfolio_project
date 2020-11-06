dfc83897a029d9a3f2581f0de0b3108ef28c6f3a
Andreas <a.iakobou@web.de>
2020-11-06 16:53:50 +0100

    Blog setup

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=40)
+    date = models.DateTimeField()
+    body = models.TextField(max_length=600)
+    image = models.ImageField(upload_to='images/')
