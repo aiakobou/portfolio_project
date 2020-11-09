8de5b11a9cc63d6bc919bfb275a8f1016577ebc4
Andreas <a.iakobou@web.de>
2020-11-09 21:01:21 +0100

    Website finished

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=40)
     date = models.DateTimeField()
-    body = models.TextField(max_length=600)
+    body = models.TextField(max_length=10000)
     image = models.ImageField(upload_to='images/')
 
     def summary(self):
